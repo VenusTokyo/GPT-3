0f193b134df172cdd5d43255c8be234bac4bff25
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-01-21 17:55:40 +0530

    added logo links and signup in nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import './navbar.css'
-import logo from "../../assets/logo.png"
+import logo from "../../assets/logo.svg"
 //BEM css is Block Element Modifier
 const Navbar = () => {
   return (
-    <div className="gpt3_navbar">
-      <div className="gp3__navbar-links">
-      <div className="gpt3__navbar-links_logo">
-        <img src={logo} alt="logo" />
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#whatgpt">What is GPT</a></p>
+          <p><a href="#possibitilty">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
       </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign In</p>
+        <button type='button'>Sign Up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        
       </div>
     </div>
   )
