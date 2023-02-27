46db41a4ec0425ea95e8060e34e63897e085f2e4
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-02-27 10:47:10 +0530

    possibility and cta complete

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import './cta.css'
 const Cta = () => {
   return (
-    <div>
-cta
+    <div className='gpt3__cta'>
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today &amp; start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt3__cta-button">
+        <button type='button'>Get Started</button>
+      </div>
     </div>
   )
 }
